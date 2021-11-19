4345e0c1e5799d2d416e17fcd91789b63e0c6150
gbengataylor <gbenga@gbenga.com>
2021-11-19 13:31:13 -0500

    update to script and readme

--- contributions/cross-team-collab-serverless/scripts/stage2-knative.sh
+++ contributions/cross-team-collab-serverless/scripts/stage2-knative.sh
@@ -3,9 +3,19 @@
 kamel run --name stage2-knative  camelk/stage-2-kafka2mail-knative.xml -d camel-jackson
 
 
-#apply Channel (optional)
-
+#KafkaSource->Service Sink
 #apply kafkaSouce
+oc apply -f  knative/question-kafka-source-service-sink.yaml
+
+##OR 
+
+#KafkaSource->Channel->Service 
+#apply Channel (optional)
+oc apply -f  knative/question-kafka-source-service-sink.yaml
+
+oc apply -f knativ/question-kafka-channel.yaml
+oc apply -f knativ/question-stage2-subscription.yaml
+
 
 #add subscription if channel was used (can also make the association via the dev console - by moving the "sink" )
 
