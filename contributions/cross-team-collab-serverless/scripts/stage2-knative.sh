ce3203ee6b757204a703a6fdabd9c3d744765f4a
gbengataylor <gbenga@gbenga.com>
2021-11-19 07:44:43 -0500

    add knative service..still need to fix the url endpoint

--- contributions/cross-team-collab-serverless/scripts/stage2-knative.sh
+++ contributions/cross-team-collab-serverless/scripts/stage2-knative.sh
@@ -1,9 +1,18 @@
 
-kamel run --name camelk/stage2-knative stage-2-kafka2mail-knative.xml -d camel-jackson
+#if knative serving is installed, the knative *should* be the default deployment controler. --trait knative.enabled=true can also be included
+kamel run --name stage2-knative  camelk/stage-2-kafka2mail-knative.xml -d camel-jackson
 
 
-#apply Channel
+#apply Channel (optional)
 
 #apply kafkaSouce
 
-#add subscription
+#add subscription if channel was used (can also make the association via the dev console )
+
+
+#current issue --> pod spins up but doesn't get the message
+
+
+#test via rest
+curl -X POST -H "content-type: application/json" -d '[ "5", "After our recent aq?", "Architecture" ]'  http://stage2-knative-demo-camelk.apps.cluster-0f5d.0f5d.sandbox1327.opentlc.com/questions
+#curl -X POST -H "content-type: application/json" -d '[ "5", "After our recent aq?", "Architecture" ]'  http://stage2-knative-demo-camelk.apps.cluster-0f5d.0f5d.sandbox1327.opentlc.com/
--- contributions/cross-team-collab-serverless/scripts/testkafkasource.sh
+++ contributions/cross-team-collab-serverless/scripts/testkafkasource.sh
@@ -1,3 +1,4 @@
+ # generate test payloads to the questions topic
  oc run kafka-producer \
     -ti --image=quay.io/strimzi/kafka:latest-kafka-2.7.0 --rm=true \
     --restart=Never -- bin/kafka-console-producer.sh \
@@ -7,7 +8,7 @@
     # ["5","After our recent aq?","Architecture"]
 
 
-#event display+
+#event display - in case your kafka source is sinking to the event display
 kn service create event-display --image quay.io/openshift-knative/knative-eventing-sources-event-display
 
 #event display log
